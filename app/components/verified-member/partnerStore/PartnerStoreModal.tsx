ffd3916a6e0b1c4800cc29eb6e0412c3957df748
edberaga <edbertjonnathan@gmail.com>
2025-09-08 19:38:09 +0700

    fix the modal alignment

--- app/components/verified-member/partnerStore/PartnerStoreModal.tsx
+++ app/components/verified-member/partnerStore/PartnerStoreModal.tsx
@@ -160,10 +160,15 @@ export default function PartnerStoreModal({
 
 const styles = StyleSheet.create({
   modalOverlay: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    zIndex: 9999,
   },
   storeModal: {
     backgroundColor: 'white',
