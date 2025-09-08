44b414a37e4f1c65b96b9bfdaba147d537a024a5
edberaga <edbertjonnathan@gmail.com>
2025-09-08 22:14:20 +0700

    Saving for later

--- components/Note.tsx
+++ components/Note.tsx
@@ -1,2 +1,2 @@
-const { data: { user } } = await supabase.auth.getUser()
-console.log('last_sign_in_at →', user?.last_sign_in_at)
+// const { data: { user } } = await supabase.auth.getUser()
+// console.log('last_sign_in_at →', user?.last_sign_in_at)
