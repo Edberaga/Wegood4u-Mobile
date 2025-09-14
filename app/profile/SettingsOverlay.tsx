fc5e05bfe1d6c7a2b886573250609ffb29591e31
Edberaga <72023119+Edberaga@users.noreply.github.com>
2025-09-14 17:26:50 +0700

    Updated SettingsOverlay.tsx

--- app/profile/SettingsOverlay.tsx
+++ app/profile/SettingsOverlay.tsx
@@ -283,6 +283,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
   logoutItem: {
     flexDirection: 'row',
     alignItems: 'center',
